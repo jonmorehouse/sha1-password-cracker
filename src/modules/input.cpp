e45304c115afd93ef12f50a0754d044ea63208eb
Jon Morehouse <morehousej09@gmail.com>
2013-03-17 14:33:11 -0700

    Build out integer input function

--- src/modules/input.cpp
+++ src/modules/input.cpp
@@ -4,15 +4,24 @@ namespace input {
 
 	int getInteger() {
 
-		
+		std::string lineData;//line data that will be grabbed so we can extract our string from it	
 
-		return 0;
+		// int input is the integer data that we will extract from the string
+		int data;
+
+		// now actually grab the user input!
+		getline(std::cin, lineData);
+
+		// now grab the actual integer from the string stream that we created
+		std::stringstream(lineData) >> data;
+
+		return data;
 	}
 
 	std::string getString() {
 
 
-		return "TEST STRING";
+		
 
 	}
 
