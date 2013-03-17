dab9e55ea45ba53280cf11d5f9a29f3d866403a8
Jon Morehouse <morehousej09@gmail.com>
2013-03-16 17:08:29 -0700

    Initialize input namespace and module

---
+++ include/modules/input.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+namespace input {
+
+	int getInteger();//get an integer from the user	
+
+	std::string getString();//get a string input from the user
+
+
+
+};
---
+++ src/modules/input.cpp
@@ -0,0 +1,21 @@
+#include "modules/input.hpp"
+
+namespace input {
+
+	int getInteger() {
+
+
+
+		return 0;
+	}
+
+	std::string getString() {
+
+
+		return "TEST STRING";
+
+	}
+
+
+};
+
